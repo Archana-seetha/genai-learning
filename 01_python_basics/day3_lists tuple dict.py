19db960275c34f1f2429c671f9011a3b961bac2b
Archana Seetha <archanasita811@gmail.com>
2026-02-02 21:30:42 +0530

    day3_lists tuple dict.py

---
+++ 01_python_basics/day3_lists tuple dict.py
@@ -0,0 +1,12 @@
+#creating list
+arc=[12,56,34,'ABC',91]
+#accessing list
+print(arc[1])
+#slicing 
+print(arc[1:4])
+#changing(mutable)
+arc[1]='abc'
+print(arc)
+#removing
+arc.remove(34)
+arc.pop()
